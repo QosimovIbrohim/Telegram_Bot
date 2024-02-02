08a7b9c0add4982bb67d2723940295bc856b17e4
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-02 20:21:28 +0500

    If changed

--- BotHandler.cs
+++ BotHandler.cs
@@ -199,6 +199,32 @@ namespace Telegram_Bot
                     {
                         CRUD.ChangeStatusCode(chatId, 0);
                     }
+                    else if (CRUD.GetStatusCode(chatId) == 2)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+                    else if (CRUD.GetStatusCode(chatId) == 3)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+                    else if (CRUD.GetStatusCode(chatId) == 4)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+                    else if (CRUD.GetStatusCode(chatId) == 5)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+                    else if (CRUD.GetStatusCode(chatId) == 6)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+                    else if (CRUD.GetStatusCode(chatId) == 7)
+                    {
+                        CRUD.ChangeStatusCode(chatId, 0);
+                    }
+
+
                 }
 
                 switch (CRUD.GetStatusCode(chatId))
@@ -226,9 +252,9 @@ namespace Telegram_Bot
                         };
 
                         await botClient.SendTextMessageAsync(
-                            chatId:chatId,
+                            chatId: chatId,
                             text: message.Text,
-                            replyMarkup:replyKeyboardMarkup,
+                            replyMarkup: replyKeyboardMarkup,
                             cancellationToken: cancellationToken);
                         break;
                     case 1:
