6a18b64fd037bd15ba579c482d1289b036c767cd
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-03 00:03:30 +0500

    Qani

--- BotHandler.cs
+++ BotHandler.cs
@@ -118,7 +118,6 @@ namespace Telegram_Bot
             {
                 if (CRUD.IsPhoneNumberNull(chatId) == false)
                 {
-                            `
                     ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
                         {
                     KeyboardButton.WithRequestContact("Contact")
