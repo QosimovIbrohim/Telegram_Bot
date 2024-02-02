053af75c58e0028e21af3374ce74bdd8d4b57ba5
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-02 18:02:15 +0500

    w

---
+++ CrudForOrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Requests;
+using TelegramBot;
+
+namespace Telegram_Bot
+{
+    public static class CrudForOrderStatus
+    {
+    }
+}
