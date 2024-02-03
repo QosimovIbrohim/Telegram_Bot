09154d85884102271991a67654af616759b2f774
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-03 16:09:42 +0500

    updated

--- CrudForBook.cs
+++ CrudForBook.cs
@@ -7,56 +7,71 @@ using System.Threading.Tasks;
 
 namespace Telegram_Bot
 {
-    public class CrudForBook
+    public class Books
     {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public long price { get; set; }
+        public string Category_name { get; set; }
+
         public static string path = @"C:\Users\user\Desktop\DatabseFolders\Books.json";
 
-           
+        public static void Create(Books bk)
+        {
+            List<Books> books = DeserializeSerialize<Books>.GetAll(path);
+            if (books.Any(c => c.Name == bk.Name))
+            {
+                return;
+            }
+            DeserializeSerialize<Books>.Save(books, path);
+        }
+        public static string Read()
+        {
+            StringBuilder builder = new StringBuilder();
+            List<Books> books = DeserializeSerialize<Books>.GetAll(path);
+            foreach (Books c in books)
+            {
+                builder.Append($"Book name:{c.Name}\n" +
+                    $"Book Author: {c.Author}\n" +
+                    $"Book Category{c.Category_name}\n" +
+                    $"Book price {c.price}\n");
+            }
+            return builder.ToString();
+        }
 
         public static void Update(string last_name, string new_price, string new_name, string new_author, string new_category_name)
         {
             try
             {
-                List<Books> books = GetAllCats();
+                List<Books> books = DeserializeSerialize<Books>.GetAll(path);
                 if (books != null)
                 {
                     int index = books.FindIndex(name => name.Name == last_name);
                     if (index != -1)
                     {
                         books[index].Name = new_name;
-                        SaveCats(books);
+                        DeserializeSerialize<Books>.Save(books, path);
                     }
                 }
             }
             catch { }
         }
 
-       
-        public static List<Books> GetAllCats()
+        public static void Delete(string del_name)
         {
+            try
+            {
+                List<Books> books = DeserializeSerialize<Books>.GetAll(path);
+                var catToRemove = books.Find(ct => ct.Category_name == del_name);
 
-            if (System.IO.File.Exists(path))
-            {
-                string json = System.IO.File.ReadAllText(path);
-                return JsonSerializer.Deserialize<List<Books>>(json) ?? new List<Books>();
+                if (catToRemove != null)
+                {
+                    books.Remove(catToRemove);
+                    DeserializeSerialize<Books>.Save(books, path);
+                }
             }
-            else
-            {
-                return new List<Books>();
-            }
+            catch { }
         }
-        public static void SaveCats(List<Books> books)
-        {
-            string json = JsonSerializer.Serialize(books);
-            System.IO.File.WriteAllText(path, json);
-        }
-    }
-    public class Books
-    {
-        public string Name { get; set; }
-        public string Author { get; set; }
-        public long price { get; set; } 
-        public string Category_name { get; set; }
     }
 }
 
