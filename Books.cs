fc1637fe3857bc07192c3654690c24b672885df5
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-04 09:03:24 +0500

    new info: add book amogi

--- Books.cs
+++ Books.cs
@@ -35,7 +35,7 @@ namespace Telegram_Bot
                 builder.Append($"Book name:{c.Name}\n" +
                     $"Book Author: {c.Author}\n" +
                     $"Book Category: {c.Category_name}\n" +
-                    $"Book price: {c.price}\n"+"\n----------------------"+"\n");
+                    $"Book price: {c.price}\n"+ "\n📖----------------------📖" + "\n");
             }
             return builder.ToString();
         }
